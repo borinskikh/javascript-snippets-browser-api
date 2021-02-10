27ebc6637913ca7d2d6c5f7d83c4f21731e3b2f9
borinskikh <causelesscause@gmail.com>
2021-02-10 23:23:15 +0300

    update

--- src/js/task1.js
+++ src/js/task1.js
@@ -1,22 +1,55 @@
 (() => {
-    const main = document.getElementById('main');
-    const task = document.createElement('div');
-    task.setAttribute('id', 'task5');
-    task.setAttribute('class', 'task');
-    const button = document.createElement('input');
-    button.setAttribute('id', 'task5-button');
-    button.setAttribute('type', 'button');
-    button.setAttribute('value', 'submit');
-    const chartWrapper = document.createElement('div');
-    chartWrapper.setAttribute('id', 'task5-chart');
-    chartWrapper.setAttribute('class', 'chart');
-    button.addEventListener('click', () => { addChart() });
+    document.getElementById('main').appendChild(getTask());
+    /*if (window.localStorage.getItem('task3-cards')) {
+        document.getElementById('task3-cards').innerHTML = window.localStorage.getItem('task3-cards');
+        console.log('Task 3: local storage was used');
+    } else {
+        document.getElementById('task3-input').value = '10';;
+        submit();
+    }*/
 
-    task.appendChild(button);
-    task.appendChild(chartWrapper);
-    main.appendChild(task);
+    function getTask() {
+        const task = document.createElement('div');
+        task.setAttribute('id', 'task1');
+        task.setAttribute('class', 'task bg-dark d-flex flex-column');
+        task.innerHTML = '<h3>task 1</h3>';
+        const content = document.createElement('div');
+        content.setAttribute('id', 'task1-content');
+        content.setAttribute('class', 'm-3 d-flex flex-column flex-lg-row flex-fill');
+        const form = document.createElement('div');
+        form.setAttribute('id', 'task1-form');
+        form.setAttribute('class', 'm-5 d-flex flex-column');
+        const field = document.createElement('div');
+        field.setAttribute('id', 'task1-field');
+        field.setAttribute('class', 'd-flex flex-column');
+        const input = document.createElement('input');
+        input.setAttribute('type', 'cardTitle');
+        input.setAttribute('id', 'task1-input');
+        input.setAttribute('class', 'bg-dark text-light border-secondary');
+        input.setAttribute('placeholder', ' Enter a number');
+        const label = document.createElement('label');
+        label.innerHTML = 'Enter a number between 1 and 10';
+        label.setAttribute('for', 'task1-input');
+        const button = document.createElement('button');
+        button.innerHTML = 'Submit';
+        button.setAttribute('id', 'task1-button');
+        button.setAttribute('class', 'm-3 btn btn-secondary');
+        button.addEventListener('click', function () { submit(); });
+        const output = document.createElement('div');
+        output.setAttribute('id', 'task1-output');
+        output.setAttribute('class', 'd-flex flex-row flex-wrap');
 
-    function addChart() {
+        task.appendChild(content);
+        content.appendChild(form);
+        content.appendChild(output);
+        form.appendChild(field);
+        form.appendChild(button);
+        field.appendChild(label);
+        field.appendChild(input);
+        return task;
+    }
+
+    function submit() {
         const chart = document.createElement('svg');
         chart.setAttribute('class', 'chart');
         chart.setAttribute('height', '200');
