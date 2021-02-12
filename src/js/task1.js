69f37c3f27c2656a8a4f30ada902e935cedcc489
borinskikh <causelesscause@gmail.com>
2021-02-12 23:44:37 +0300

    task2 works

--- src/js/task1.js
+++ src/js/task1.js
@@ -3,9 +3,12 @@
 
     function getTask() {
         const ns = 'http://www.w3.org/2000/svg';
+        const tasks = document.createElement('div');
+        tasks.setAttribute('id', 'tasks');
+        tasks.setAttribute('class', 'task bg-dark d-flex flex-row flex-wrap');
         const task = document.createElement('div');
         task.setAttribute('id', 'task1');
-        task.setAttribute('class', 'task bg-dark d-flex flex-column');
+        task.setAttribute('class', 'd-flex flex-column');
         task.innerHTML = '<h3>task 1</h3>';
         const content = document.createElement('div');
         content.setAttribute('id', 'task1-content');
@@ -30,13 +33,14 @@
         path.setAttribute('d', 'M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-5.904-2.854a.5.5 0 1 1 .707.708L6.707 9.95h2.768a.5.5 0 1 1 0 1H5.5a.5.5 0 0 1-.5-.5V6.475a.5.5 0 1 1 1 0v2.768l4.096-4.097z');
         icon.appendChild(path);
 
+        tasks.appendChild(task);
         task.appendChild(content);
         content.appendChild(form);
         form.appendChild(button);
         button.appendChild(icon);
         button.appendChild(document.createTextNode('Submit'));
 
-        return task;
+        return tasks;
     }
 
     function submit() {
